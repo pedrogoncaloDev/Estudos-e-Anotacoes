af8eea065522b5db4e804873dd6f431f846df4cf
PedroGoncaloGRV <pedro.henrique@grvsoftware.com.br>
2025-02-10 10:11:51 -0300

    removido o akita, vou usar Signal

--- Angular/cadastro-funcionarios/src/main.ts
+++ Angular/cadastro-funcionarios/src/main.ts
@@ -1,11 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { enableAkitaProdMode } from '@datorama/akita';
-import { isDevMode } from '@angular/core';
-
-if (!isDevMode()) {
-  enableAkitaProdMode();
-}
 
 bootstrapApplication(AppComponent);
