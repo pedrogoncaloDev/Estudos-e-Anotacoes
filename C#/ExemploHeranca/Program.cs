7edf0a1bce47a1690f6eb17313f69f4da4766f6a
Pedro Gonçalo <pedro.gonsalo939@gmail.com>
2025-10-15 23:12:30 -0300

    feat: add ExemploHeranca e ExamploPolimorfismo

---
+++ C#/ExemploHeranca/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ExemploHeranca
+{
+    // Classe base (Pai)
+    public class Animal
+    {
+        public string Nome { get; set; }
+
+        public void Comer()
+        {
+            Console.WriteLine($"{Nome} está comendo.");
+        }
+    }
+
+    // Classe derivada (Filha)
+    public class Cachorro : Animal
+    {
+        public void Latir()
+        {
+            Console.WriteLine($"{Nome} está latindo: Au Au!");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            // Criando um objeto da classe filha
+            Cachorro dog = new Cachorro();
+            dog.Nome = "Rex";
+
+            // Métodos herdados da classe pai
+            dog.Comer();
+
+            // Método próprio da classe filha
+            dog.Latir();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/ExemploPolimorfismo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ExemploPolimorfismo
+{
+    // Classe base
+    public class Animal
+    {
+        public virtual void Falar()
+        {
+            Console.WriteLine("O animal faz um som...");
+        }
+    }
+
+    // Classe filha: Gato
+    public class Gato : Animal
+    {
+        public override void Falar()
+        {
+            Console.WriteLine("O gato mia: Miau!");
+        }
+    }
+
+    // Classe filha: Vaca
+    public class Vaca : Animal
+    {
+        public override void Falar()
+        {
+            Console.WriteLine("A vaca muge: Muuu!");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Animal[] animais = new Animal[]
+            {
+                new Cachorro(),
+                new Gato(),
+                new Vaca()
+            };
+
+            foreach (var animal in animais)
+            {
+                animal.Falar();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
